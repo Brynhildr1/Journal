11a2b89f767712c0a2ec0b994ead02d522f049ba
Brynhildr1 <67852486+Brynhildr1@users.noreply.github.com>
2023-05-23 02:29:10 +1000

    Complete User login / match entry ID to user

--- main.cpp
+++ main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <stdio.h>
 #include "sqlite3.h"
+#include <functional>
 
 using namespace std;
 
 static int createDB(const char *s);
-static int createTable(const char *s, string sql);
+static int createTable(const char *s, const string &sql);
 static int displayEntries(const char *s);
+static pair<string, int> createUser(const char *s);
+static pair<string, int> login(const char *s);
 
-// Create a callback function
-static int callback(void *data, int argc, char **argv, char **azColName)
-{
-    int i;
-    fprintf(stderr, "%s: ", (const char *)data);
-
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
-}
-
-// stores all the information for the journal entry
 struct JournalEntry
 {
     string title;
@@ -33,14 +20,33 @@ struct JournalEntry
     string date;
 };
 
-vector<JournalEntry> journalEntries;
-
-// gathering the data from the user for the entry
-void createEntry(const char *s)
+struct JournalUser
 {
+    string username;
+    string password;
+};
 
+static int callback(void *data, int argc, char **argv, char **azColName)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+static int callbackCount(void *data, int argc, char **argv, char **azColName)
+{
+    int *count = static_cast<int *>(data);
+    if (argc > 0 && argv[0])
+        *count = atoi(argv[0]);
+    return 0;
+}
+
+void createEntry(const char *s, int loggedUserID)
+{
     JournalEntry entry;
-    sqlite3 *DB;
 
     cout << "Title: ";
     getline(cin >> ws, entry.title);
@@ -51,83 +57,112 @@ void createEntry(const char *s)
     cout << "Tags: ";
     getline(cin >> ws, entry.tags);
 
-    /*     cout << "Date: ";
-        getline(cin >> ws, entry.date); */
+    string sql = "INSERT INTO ENTRIES(UserID, TITLE, CONTENT, TAGS) VALUES(" + to_string(loggedUserID) + ",'" + entry.title + "','" + entry.content + "','" + entry.tags + "');";
 
-    string sql = "INSERT INTO ENTRIES(TITLE, CONTENT, TAGS) VALUES('" + entry.title + "','" + entry.content + "','" + entry.tags + "');";
-
-    try
+    sqlite3 *DB;
+    int rc = sqlite3_open(s, &DB);
+    if (rc != SQLITE_OK)
     {
-        int exit = 0;
-        exit = sqlite3_open(s, &DB);
-
-        char *messageError;
-        exit = sqlite3_exec(DB, sql.c_str(), callback, 0, &messageError);
-
-        if (exit != SQLITE_OK)
-        {
-            cerr << "Error creating Entry" << endl;
-            sqlite3_free(messageError);
-        }
-        else
-            cout << "Entry created successfully" << endl;
+        cerr << "Error opening database: " << sqlite3_errmsg(DB) << endl;
         sqlite3_close(DB);
+        return;
     }
-    catch (const exception &e)
+
+    rc = sqlite3_exec(DB, sql.c_str(), callback, 0, nullptr);
+    if (rc != SQLITE_OK)
     {
-        cerr << e.what();
+        cerr << "Error creating Entry: " << sqlite3_errmsg(DB) << endl;
     }
+    else
+    {
+        cout << "Entry created successfully" << endl;
+    }
+
+    sqlite3_close(DB);
 }
-
-// displays the stored entries, this will need to be learnt and probably changed alot
-/* void displayEntries()
-{
-    cout << "------ Journal Entries ------" << endl;
-
-    for (const auto &entry : journalEntries)
-    {
-        cout << "Title: " << entry.title << endl;
-        cout << "Content: " << entry.content << endl;
-        cout << "Tags: " << entry.tags << endl;
-        cout << "Date: " << entry.date << endl;
-        cout << "----------------------------" << endl;
-    }
-
-    if (journalEntries.empty())
-    {
-        cout << "No entries found." << endl;
-    }
-} */
 
 int main()
 {
     const char *dir = "C:\\Users\\ellia\\Desktop\\coding\\Journal\\journal_database.db";
-
-    sqlite3 *DB;
-
-    // First time set up for database and tables if they dont already exist
+    JournalUser user;
     createDB(dir);
-
-    // Create SQL statement, One time only if doesnt exist
     createTable(dir, "CREATE TABLE IF NOT EXISTS ENTRIES("
                      "ID INTEGER NOT NULL PRIMARY KEY,"
+                     "USERID INTEGER NOT NULL,"
                      "TITLE TEXT NOT NULL,"
                      "CONTENT TEXT NOT NULL,"
                      "TAGS TEXT NOT NULL);");
     createTable(dir, "CREATE TABLE IF NOT EXISTS USER("
                      "ID INTEGER NOT NULL PRIMARY KEY,"
-                     "EMAIL NOT NULL,"
+                     "USERNAME NOT NULL,"
                      "PASSWORD NOT NULL);");
 
-    // user choice to add ar view or exit.
-    int choice;
+    // Get user login or make new account.
+    int choice = 0;
+    string username;
+    bool loginKey = false;
+
+    string loggedUsername;
+    int loggedUserID = -1;
 
     do
     {
         cout << "----- Journal Application -----" << endl;
-        cout << "1. Create Entry" << endl;
-        cout << "2. Display Entries" << endl;
+        cout << "1. Existing User" << endl;
+        cout << "2. New User" << endl;
         cout << "3. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cout << "\n----------------------------------\n\n";
+
+        switch (choice)
+        {
+        case 1:
+        {
+            // user logs in
+            pair<string, int> loginResult = login(dir);
+            if (!loginResult.first.empty() && loginResult.second != -1)
+            {
+                loggedUsername = loginResult.first;
+                loggedUserID = loginResult.second;
+                loginKey = true;
+            }
+            break;
+        }
+        case 2:
+        {
+            // user creates account
+            cout << "Directing to account creation..." << endl
+                 << endl;
+            pair<string, int> userInfo = createUser(dir);
+            if (userInfo.second != -1)
+            {
+                loggedUsername = userInfo.first;
+                loggedUserID = userInfo.second;
+                loginKey = true;
+            }
+            break;
+        }
+        case 3:
+            cout << "Exiting the application. Goodbye!" << endl;
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
+
+        cout << endl;
+    } while (loginKey == false);
+
+    choice = 0;
+
+    while (choice != 3)
+    {
+        cout << "----------------------------------\n\n";
+        cout << "Logged ID: " << loggedUserID << ", User: " << loggedUsername << endl;
+        cout << "1. Create Entry\n";
+        cout << "2. Display Entries\n";
+        cout << "3. Logout/Exit\n";
+
         cout << "Enter your choice: ";
         cin >> choice;
         cout << endl;
@@ -135,20 +170,20 @@ int main()
         switch (choice)
         {
         case 1:
-            createEntry(dir);
+            createEntry(dir, loggedUserID);
             break;
         case 2:
             displayEntries(dir);
             break;
         case 3:
-            cout << "Exiting the application. Goodbye!" << endl;
-            break;
+            cout << "Exiting the application. Goodbye!\n";
+            return 0;
         default:
-            cout << "Invalid choice. Please try again." << endl;
+            cout << "Invalid choice. Please try again.\n";
         }
 
         cout << endl;
-    } while (choice != 3);
+    }
 
     int x;
     cin >> x;
@@ -156,41 +191,39 @@ int main()
     return 0;
 }
 
-// function used to create the database if one doesnt exist at the dir mentioned in main
 static int createDB(const char *s)
 {
     sqlite3 *DB;
-    int exit = 0;
+    int rc = 0;
 
-    exit = sqlite3_open(s, &DB);
+    rc = sqlite3_open(s, &DB);
 
     sqlite3_close(DB);
 
     return 0;
 }
 
-// function used to create the tables with in the database assuming that they do not already exist.
-static int createTable(const char *s, string sql)
+static int createTable(const char *s, const string &sql)
 {
     sqlite3 *DB;
 
     try
     {
-        int exit = 0;
-        exit = sqlite3_open(s, &DB);
-        char *messageError;
+        int rc = 0;
+        rc = sqlite3_open(s, &DB);
 
         // execute SQL statement
-        exit = sqlite3_exec(DB, sql.c_str(), callback, 0, &messageError);
+        rc = sqlite3_exec(DB, sql.c_str(), callback, 0, nullptr);
 
         // Error check
-        if (exit != SQLITE_OK)
+        if (rc != SQLITE_OK)
         {
             cerr << "Error creating Table" << endl;
-            sqlite3_free(messageError);
         }
         else
+        {
             sqlite3_close(DB);
+        }
     }
     catch (const exception &e)
     {
@@ -202,19 +235,17 @@ static int createTable(const char *s, string sql)
 static int displayEntries(const char *s)
 {
     sqlite3 *DB;
-    int exit = 0;
-    exit = sqlite3_open(s, &DB);
+    int rc = 0;
+    rc = sqlite3_open(s, &DB);
     string sql = "SELECT * FROM ENTRIES";
-    char *messageError;
     const char *data = "Callback function called";
 
     /* Execute SQL statement */
-    exit = sqlite3_exec(DB, sql.c_str(), callback, (void *)data, &messageError);
+    rc = sqlite3_exec(DB, sql.c_str(), callback, (void *)data, nullptr);
 
-    if (exit != SQLITE_OK)
+    if (rc != SQLITE_OK)
     {
-        fprintf(stderr, "SQL error: %s\n", messageError);
-        sqlite3_free(messageError);
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(DB));
     }
     else
     {
@@ -225,137 +256,150 @@ static int displayEntries(const char *s)
     return 0;
 }
 
-/* // Create a callback function
-int callback(void *NotUsed, int argc, char **argv, char **azColName)
+static pair<string, int> createUser(const char *s)
 {
-    // Return successful
-    return 0;
-}
+    JournalUser user;
+    sqlite3 *DB;
+    int rc = 0;
+    rc = sqlite3_open(s, &DB);
 
-int main()
-{
+    cout << "Username: ";
+    getline(cin >> ws, user.username);
 
-    // Pointer to SQLite connection
-    sqlite3 *db;
+    cout << "Password: ";
+    getline(cin >> ws, user.password);
 
-    // Save any error messages
-    char *zErrMsg = 0;
+    string checkQuery = "SELECT COUNT(*) FROM USER WHERE USERNAME = '" + user.username + "';";
+    string insertQuery = "INSERT INTO USER(USERNAME, PASSWORD) VALUES('" + user.username + "','" + user.password + "');";
 
-    // Save the result of opening the file
-    int rc;
+    char *zErrMsg;
+    int count = 0;
 
-    // Save any SQL
-    string sql;
+    rc = sqlite3_exec(DB, checkQuery.c_str(), callbackCount, &count, &zErrMsg);
 
-    // Save the result of opening the file
-    rc = sqlite3_open("journal_database.db", &db);
-
-    if (rc)
+    if (rc != SQLITE_OK)
     {
-        // Show an error message
-        cout << "DB Error: " << sqlite3_errmsg(db) << endl;
-        // Close the connection
-        sqlite3_close(db);
-        // Return an error
-        return (1);
+        cerr << "Error creating User" << endl;
+        sqlite3_free(zErrMsg);
     }
-
-    // Save SQL to create a table
-    sql = "CREATE TABLE USER ("
-          "ID INT PRIMARY KEY     NOT NULL,"
-          "NAME           TEXT    NOT NULL,"
-          "PASSWORD     iNTEGER   NOT NULL);";
-
-    // Run the SQL (convert the string to a C-String with c_str() )
-    rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-
-    // Close the SQL connection
-    sqlite3_close(db);
-
-    return 0;
-} */
-
-/* struct JournalEntry
-{
-    string title;
-    string content;
-    string tags;
-    string date;
-};
-
-vector<JournalEntry> journalEntries;
-
-void createEntry()
-{
-    JournalEntry entry;
-
-    cout << "Title: ";
-    getline(cin >> ws, entry.title);
-
-    cout << "Content: ";
-    getline(cin >> ws, entry.content);
-
-    cout << "Tags: ";
-    getline(cin >> ws, entry.tags);
-
-    cout << "Date: ";
-    getline(cin >> ws, entry.date);
-
-    journalEntries.push_back(entry);
-
-    cout << "Entry created successfully!" << endl;
-}
-
-void displayEntries()
-{
-    cout << "------ Journal Entries ------" << endl;
-
-    for (const auto &entry : journalEntries)
+    else if (count > 0)
     {
-        cout << "Title: " << entry.title << endl;
-        cout << "Content: " << entry.content << endl;
-        cout << "Tags: " << entry.tags << endl;
-        cout << "Date: " << entry.date << endl;
-        cout << "----------------------------" << endl;
+        cerr << "Username already exists" << endl;
     }
-
-    if (journalEntries.empty())
+    else
     {
-        cout << "No entries found." << endl;
-    }
-}
+        rc = sqlite3_exec(DB, insertQuery.c_str(), nullptr, nullptr, &zErrMsg);
 
-int main()
-{
-    int choice;
-
-    do
-    {
-        cout << "----- Journal Application -----" << endl;
-        cout << "1. Create Entry" << endl;
-        cout << "2. Display Entries" << endl;
-        cout << "3. Exit" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
+        if (rc != SQLITE_OK)
         {
-        case 1:
-            createEntry();
-            break;
-        case 2:
-            displayEntries();
-            break;
-        case 3:
-            cout << "Exiting the application. Goodbye!" << endl;
-            break;
-        default:
-            cout << "Invalid choice. Please try again." << endl;
+            cerr << "Error creating User" << endl;
+            sqlite3_free(zErrMsg);
         }
+        else
+        {
+            cout << "User created successfully" << endl;
+            int userID = sqlite3_last_insert_rowid(DB);
+            sqlite3_close(DB);
+            return make_pair(user.username, userID);
+        }
+    }
 
-        cout << endl;
-    } while (choice != 3);
+    sqlite3_close(DB);
 
-    return 0;
+    return make_pair("", -1);
 }
- */
+
+static pair<string, int> login(const char *s)
+{
+    string username;
+    sqlite3 *DB;
+    int rc = 0;
+    rc = sqlite3_open(s, &DB);
+
+    cout << "Username: ";
+    getline(cin >> ws, username);
+
+    string checkQuery = "SELECT COUNT(*) FROM USER WHERE USERNAME = '" + username + "';";
+    string selectQuery = "SELECT ID FROM USER WHERE USERNAME = '" + username + "';";
+    char *zErrMsg;
+    int count = 0;
+    int userID = -1;
+
+    rc = sqlite3_exec(
+        DB, checkQuery.c_str(), [](void *data, int argc, char **argv, char **azColName)
+        {
+        int *count = static_cast<int *>(data);
+        if (argc > 0 && argv[0])
+            *count = atoi(argv[0]);
+        return 0; },
+        &count, &zErrMsg);
+
+    if (rc != SQLITE_OK)
+    {
+        cerr << "Error accessing User table" << endl;
+        sqlite3_free(zErrMsg);
+    }
+    else if (count == 0)
+    {
+        cerr << "Username does not exist. Would you like to create a new account? (Y/N): ";
+        string response;
+        getline(cin >> ws, response);
+        if (response == "Y" || response == "y")
+        {
+            return createUser(s);
+        }
+    }
+    else
+    {
+        string password;
+        cout << "Password: ";
+        getline(cin >> ws, password);
+
+        string passwordQuery = "SELECT COUNT(*) FROM USER WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "';";
+
+        rc = sqlite3_exec(
+            DB, passwordQuery.c_str(), [](void *data, int argc, char **argv, char **azColName)
+            {
+            int *count = static_cast<int *>(data);
+            if (argc > 0 && argv[0])
+                *count = atoi(argv[0]);
+            return 0; },
+            &count, &zErrMsg);
+
+        if (rc != SQLITE_OK)
+        {
+            cerr << "Error accessing User table" << endl;
+            sqlite3_free(zErrMsg);
+        }
+        else if (count == 0)
+        {
+            cerr << "Incorrect password" << endl;
+        }
+        else
+        {
+            rc = sqlite3_exec(
+                DB, selectQuery.c_str(), [](void *data, int argc, char **argv, char **azColName)
+                {
+                int *userID = static_cast<int *>(data);
+                if (argc > 0 && argv[0])
+                    *userID = atoi(argv[0]);
+                return 0; },
+                &userID, &zErrMsg);
+
+            if (rc != SQLITE_OK)
+            {
+                cerr << "Error accessing User table" << endl;
+                sqlite3_free(zErrMsg);
+            }
+            else
+            {
+                cout << "Login successful" << endl;
+                sqlite3_close(DB);
+                return make_pair(username, userID);
+            }
+        }
+    }
+
+    sqlite3_close(DB);
+    return make_pair("", -1);
+}
